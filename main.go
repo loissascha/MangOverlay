e5020dc8133232b51cf207ae22d685181791127b
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-09-07 11:51:51 +0200

    update window title

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "mangohud-configurator",
+		Title:  "MangOverlay",
 		Width:  924,
 		Height: 998,
 		AssetServer: &assetserver.Options{
