f41adb23db4bca4bfab5ed2d43971ce063cfc869
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-09-19 11:47:46 +0200

    fps limits view

--- frontend/src/pages/FpsLimitSettings.tsx
+++ frontend/src/pages/FpsLimitSettings.tsx
@@ -1,9 +1,45 @@
+import { faMinus, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useEffect, useState } from "react"
+import { AddFPSLimit, GetFPSLimits } from "../../wailsjs/go/main/App"
+import SettingBox from "../ui/SettingBox"
 
 function FpsLimitSettings() {
+    const [fpsLimits, setFpsLimits] = useState<any>([])
+
+    useEffect(() => {
+        GetFPSLimits().then((r) => {
+            console.log(r)
+            setFpsLimits(r)
+        })
+    }, [])
+
     return (
         <>
-            <div className="flex gap-5 flex-auto flex-wrap">
+            <div className="grid grid-cols-2 gap-3">
                 <div>
+                    <SettingBox header="FPS Limits">
+                        <div className="flex gap-3 justify-end">
+                            <a onClick={() => {
+                                AddFPSLimit("0").then(() => {
+                                    GetFPSLimits().then((r) => {
+                                        setFpsLimits(r)
+                                    })
+                                })
+                            }} className="cursor-pointer"><FontAwesomeIcon icon={faPlus} /></a>
+                        </div>
+                        <div>
+                            {fpsLimits.map((limit: any, index: number) => (
+                                <div key={index}>
+                                    {limit}
+                                </div>
+                            ))}
+                        </div>
+                    </SettingBox>
+                </div>
+                <div>
+                    <SettingBox header="Config">
+                    </SettingBox>
                 </div>
             </div>
         </>
