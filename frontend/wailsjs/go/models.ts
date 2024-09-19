d5b07180ce3e06f10a845a9b0d4027712c5764ed
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-09-19 19:53:21 +0200

    update to wails 2.9.2

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,7 +1,9 @@
 export namespace config {
 	
 	export class Element {
-	
+	    Name: string;
+	    Active: boolean;
+	    Index: number;
 	
 	    static createFrom(source: any = {}) {
 	        return new Element(source);
@@ -9,7 +11,9 @@ export namespace config {
 	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
-	
+	        this.Name = source["Name"];
+	        this.Active = source["Active"];
+	        this.Index = source["Index"];
 	    }
 	}
 
