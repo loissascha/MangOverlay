608933e383f408a22b565e59d735faa70f3fbf7b
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-09-21 22:24:39 +0200

    show all the graphs toggles

--- frontend/src/generalSettings/Graphs.tsx
+++ frontend/src/generalSettings/Graphs.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { GetGraphCpuLoad, GetGraphCpuTemp, GetGraphGpuCoreClock, GetGraphGpuLoad, GetGraphGpuMemClock, GetGraphGpuTemp, GetGraphRam, GetGraphVram, ToggleGraphGpuLoad } from "../../wailsjs/go/main/App"
+import { GetGraphCpuLoad, GetGraphCpuTemp, GetGraphGpuCoreClock, GetGraphGpuLoad, GetGraphGpuMemClock, GetGraphGpuTemp, GetGraphRam, GetGraphVram, ToggleGraphCpuLoad, ToggleGraphCpuTemp, ToggleGraphGpuCoreClock, ToggleGraphGpuLoad, ToggleGraphGpuMemClock, ToggleGraphGpuTemp, ToggleGraphRam, ToggleGraphVram } from "../../wailsjs/go/main/App"
 import SettingBox from "../ui/SettingBox"
 
 function Graphs() {
@@ -46,17 +46,110 @@ function Graphs() {
     return (
         <SettingBox header="Graphs">
             <div className="grid grid-cols-2 gap-3">
-                <label htmlFor="gpuload" className="me-2">GPU Load:</label>
-                <input
-                    type="checkbox"
-                    id="gpuload"
-                    checked={gpuLoad}
-                    onChange={() => {
-                        ToggleGraphGpuLoad().then(() => {
-                            updateGraphs()
-                        })
-                    }}
-                />
+                <label htmlFor="gpuload" className="cursor-pointer me-2">GPU Load:</label>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="gpuload"
+                        checked={gpuLoad}
+                        onChange={() => {
+                            ToggleGraphGpuLoad().then(() => {
+                                updateGraphs()
+                            })
+                        }}
+                    />
+                </div>
+                <label htmlFor="cpuload" className="cursor-pointer me-2">CPU Load:</label>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="cpuload"
+                        checked={cpuLoad}
+                        onChange={() => {
+                            ToggleGraphCpuLoad().then(() => {
+                                updateGraphs()
+                            })
+                        }}
+                    />
+                </div>
+                <label htmlFor="gpu_core_clock" className="cursor-pointer me-2">GPU Core Clock:</label>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="gpu_core_clock"
+                        checked={gpuCoreClock}
+                        onChange={() => {
+                            ToggleGraphGpuCoreClock().then(() => {
+                                updateGraphs()
+                            })
+                        }}
+                    />
+                </div>
+                <label htmlFor="gpu_mem_clock" className="cursor-pointer me-2">GPU Mem Clock:</label>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="gpu_mem_clock"
+                        checked={gpuMemClock}
+                        onChange={() => {
+                            ToggleGraphGpuMemClock().then(() => {
+                                updateGraphs()
+                            })
+                        }}
+                    />
+                </div>
+                <label htmlFor="vram" className="cursor-pointer me-2">Vram:</label>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="vram"
+                        checked={vram}
+                        onChange={() => {
+                            ToggleGraphVram().then(() => {
+                                updateGraphs()
+                            })
+                        }}
+                    />
+                </div>
+                <label htmlFor="ram" className="cursor-pointer me-2">Ram:</label>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="ram"
+                        checked={ram}
+                        onChange={() => {
+                            ToggleGraphRam().then(() => {
+                                updateGraphs()
+                            })
+                        }}
+                    />
+                </div>
+                <label htmlFor="cpu_temp" className="cursor-pointer me-2">CPU Temp:</label>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="cpu_temp"
+                        checked={cpuTemp}
+                        onChange={() => {
+                            ToggleGraphCpuTemp().then(() => {
+                                updateGraphs()
+                            })
+                        }}
+                    />
+                </div>
+                <label htmlFor="gpu_temp" className="cursor-pointer me-2">GPU Temp:</label>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="gpu_temp"
+                        checked={gpuTemp}
+                        onChange={() => {
+                            ToggleGraphGpuTemp().then(() => {
+                                updateGraphs()
+                            })
+                        }}
+                    />
+                </div>
             </div>
         </SettingBox>
     )
