51f838b365fb3486fce9825c4007a402dbb58b37
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-09-24 16:38:16 +0200

    list custom config lines

--- frontend/src/pages/AdvancedSettings.tsx
+++ frontend/src/pages/AdvancedSettings.tsx
@@ -1,5 +1,49 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useEffect, useState } from "react"
+import { GetUnorderedElements } from "../../wailsjs/go/main/App"
+import SettingBox from "../ui/SettingBox"
+
 function AdvancedSettings() {
+    const [elements, setElements] = useState<any>([])
+
+    useEffect(() => {
+        reloadUnorderedElements()
+    }, [])
+
+    async function reloadUnorderedElements() {
+        const r = await GetUnorderedElements()
+        console.log(r)
+        setElements(r)
+    }
+
     return (<>
+        <div className="flex gap-5 flex-auto flex-wrap">
+            <div>
+                <SettingBox header="Add custom lines in config">
+                    <div>
+                        <div className="flex gap-3">
+                            <div>
+                                <input type="text"
+                                    className="bg-gray-700 border border-gray-500 p-2 rounded w-60"
+                                />
+                            </div>
+                            <div className="flex items-center">
+                                <a onClick={() => {
+                                }} className="cursor-pointer text-lg"><FontAwesomeIcon icon={faPlus} /></a>
+                            </div>
+                        </div>
+                        <div>
+                            {elements.map((e: any) => (
+                                <div key={e.Index}>
+                                    {e.Name}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </SettingBox>
+            </div>
+        </div>
     </>)
 }
 export default AdvancedSettings
