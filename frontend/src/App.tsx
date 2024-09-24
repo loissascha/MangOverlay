348741134ab48ed55e53b24967db484f1954a30a
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-09-24 15:50:57 +0200

    refactor and advanced settings nav and page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { act, useEffect, useState } from "react"
 import GeneralSettings from "./pages/GeneralSettings";
 import { DisableGlobally, EnableGlobally, GloballyEnabled, RestartVkcube } from "../wailsjs/go/main/App";
 import Button from "./ui/Button";
@@ -7,8 +7,9 @@ import MetricsSettings from "./pages/MetricsSettings";
 import FpsLimitSettings from "./pages/FpsLimitSettings";
 import MetricsOrderSettings from "./pages/MetricsOrderSettings";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCheck, faDisplay, faGears, faKeyboard, faSort } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCheck, faDisplay, faGears, faKeyboard, faPlus, faSort, faWrench } from "@fortawesome/free-solid-svg-icons";
 import { faSquare, faSquareCheck } from "@fortawesome/free-regular-svg-icons";
+import AdvancedSettings from "./pages/AdvancedSettings";
 
 function App() {
     const [activeMenu, setActiveMenu] = useState<string>("general");
@@ -61,23 +62,33 @@ function App() {
                         </div>
                         <a className="hidden md:inline-block">FPS Limits</a>
                     </li>
+                    <li
+                        onClick={() => { setActiveMenuButton("manual"); }}
+                        className={"pt-2 pb-1 px-3 cursor-pointer rounded-t-lg flex gap-2 " + (activeMenu == "manual" ? "bg-gray-800" : "bg-gray-900")}>
+                        <div>
+                            <FontAwesomeIcon icon={faWrench} title="Advanced Config" />
+                        </div>
+                        <a className="hidden md:inline-block">Advanced Config</a>
+                    </li>
                 </ul>
             </nav>
             <main className="bg-gray-800 p-3">
-                {
-                    activeMenu == "general" ?
-                        (<GeneralSettings />) :
-                        activeMenu == "keybinds" ?
-                            (<KeybindSettings />) :
-                            activeMenu == "elements" ?
-                                (<ElementsSettings />) :
-                                activeMenu == "metrics" ?
-                                    (<MetricsSettings />) :
-                                    activeMenu == "metrics-order" ?
-                                        (<MetricsOrderSettings />) :
-                                        activeMenu == "fpslimits" ?
-                                            (<FpsLimitSettings />) : null
-                }
+                {(() => {
+                    switch (activeMenu) {
+                        case "general":
+                            return <GeneralSettings />
+                        case "metrics":
+                            return <MetricsSettings />
+                        case "metrics-order":
+                            return <MetricsOrderSettings />
+                        case "fpslimits":
+                            return <FpsLimitSettings />
+                        case "manual":
+                            return <AdvancedSettings />
+                        default:
+                            return null
+                    }
+                })()}
             </main>
             <footer className="bg-gray-700 p-2">
                 <Button click={() => { RestartVkcube(); }}>
---
+++ frontend/src/pages/AdvancedSettings.tsx
@@ -0,0 +1,5 @@
+function AdvancedSettings() {
+    return (<>
+    </>)
+}
+export default AdvancedSettings
