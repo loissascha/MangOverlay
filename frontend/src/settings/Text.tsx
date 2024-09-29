fdfa71b32a8430bb6f62c08421b33caf79834141
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-09-29 17:21:39 +0200

    update text popover

--- frontend/src/settings/Text.tsx
+++ frontend/src/settings/Text.tsx
@@ -1,16 +1,15 @@
 import SettingBox from "../ui/SettingBox";
-import { GetOrientation, GetRoundCorners, GetBackgroundAlpha, GetBackgroundColor, GetFontSize, SetBackgroundColor, SetBackgroundAlpha, SetRoundedCorners, SetFontSize, SetOrientation, GetTextColor, SetTextColor, GetCpuLoadColor0, GetCpuLoadColor1, GetCpuLoadColor2, SetCpuLoadColors, GetGpuLoadColor0, GetGpuLoadColor1, GetGpuLoadColor2, SetGpuLoadColors } from "../../wailsjs/go/main/App";
+import { GetFontSize, SetFontSize, GetTextColor, SetTextColor } from "../../wailsjs/go/main/App";
 import { useEffect, useState } from "react";
 import { SketchPicker } from "react-color";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 
 function Text() {
     const [textColor, setTextColor] = useState<string>("");
-    const [showTextPicker, setShowTextPicker] = useState<boolean>(false);
 
     const defaultTextColor = "FFFFFF"
-
 
     const [fontSize, setFontSize] = useState<string>("");
 
@@ -49,17 +48,21 @@ function Text() {
                 <label htmlFor="textcolor" className="me-2">Color:</label>
                 <div>
                     <div className="flex gap-2">
-                        <button
-                            style={{
+                        <Popover className="relative">
+                            <PopoverButton style={{
                                 backgroundColor: "#" + textColor
-                            }}
-                            onClick={() => {
-                                setShowTextPicker(!showTextPicker)
-                            }}
-                            className="cursor-pointer p-1 rounded border border-gray-500 w-28"
-                        >
-                            {textColor}
-                        </button>
+                            }} className="cursor-pointer p-1 rounded border border-gray-500 w-28">{textColor}</PopoverButton>
+                            <PopoverPanel anchor="bottom" className="flex flex-col bg-gray-500">
+                                <SketchPicker color={textColor} onChange={(color) => {
+                                    let col = color.hex;
+                                    if (col[0] == "#") {
+                                        col = col.substring(1)
+                                    }
+                                    setTextColor(col)
+                                    SetTextColor(col)
+                                }} />
+                            </PopoverPanel>
+                        </Popover>
                         {textColor != defaultTextColor && (
                             <button
                                 onClick={() => {
@@ -70,20 +73,6 @@ function Text() {
                             </button>
                         )}
                     </div>
-                    {showTextPicker ? (
-                        <div className="absolute z-50 text-black">
-                            <div className="fixed top-0 left-0 right-0 bottom-0" onClick={() => {
-                                setShowTextPicker(false)
-                            }}> </div>
-                            <SketchPicker color={textColor} onChange={(color) => {
-                                let col = color.hex;
-                                if (col[0] == "#") {
-                                    col = col.substring(1)
-                                }
-                                setTextColor(col)
-                                SetTextColor(col)
-                            }} />
-                        </div>) : null}
                 </div>
             </div>
         </SettingBox>
