be422120eda68c9d6c713831ede9b95f245f2551
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-01-27 13:03:34 +0100

    improved modal looks

--- frontend/src/ui/Modal.tsx
+++ frontend/src/ui/Modal.tsx
@@ -4,8 +4,8 @@ interface Props {
 
 function Modal({ children }: Props) {
   return (
-    <div className="fixed left-0 right-0 top-0 bottom-0 bg-black bg-opacity-50 content-center">
-      <div className="w-96 max-w-full bg-latte-base dark:bg-mocha-base mx-auto px-8 py-6 rounded overflow-y-auto max-h-full">
+    <div className="fixed left-0 right-0 top-0 bottom-0 bg-black bg-opacity-70 content-center">
+      <div className="w-96 max-w-full bg-latte-base dark:bg-mocha-base mx-auto px-8 py-6 rounded overflow-y-auto max-h-full border border-latte-surface1 dark:border-mocha-surface1">
         {children}
       </div>
     </div>
