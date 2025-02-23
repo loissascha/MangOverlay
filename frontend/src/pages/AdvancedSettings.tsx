be4906e241f73fd3e5361040e6887136009941fe
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-02-23 11:28:21 +0100

    advanced settings refactor

--- frontend/src/pages/AdvancedSettings.tsx
+++ frontend/src/pages/AdvancedSettings.tsx
@@ -1,15 +1,16 @@
-import FpsLimits from "../settings/FpsLimits"
-import UserLines from "../settings/UserLines"
+import FpsLimits from "../settings/FpsLimits";
+import UserLines from "../settings/UserLines";
 
 function AdvancedSettings() {
-
-    return (<>
-        <div className="flex gap-5 flex-auto flex-wrap">
-            <div className="flex flex-col gap-3">
-                <UserLines />
-                <FpsLimits />
-            </div>
+  return (
+    <>
+      <div>
+        <div className="flex flex-col gap-3">
+          <UserLines />
+          <FpsLimits />
         </div>
-    </>)
+      </div>
+    </>
+  );
 }
-export default AdvancedSettings
+export default AdvancedSettings;
--- frontend/src/settings/UserLines.tsx
+++ frontend/src/settings/UserLines.tsx
@@ -1,77 +1,89 @@
-import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useEffect, useRef, useState } from "react"
-import { AddUnorderedElement, GetUnorderedElements, RemoveUnorderedElement } from "../../wailsjs/go/main/App"
-import SettingBox from "../ui/SettingBox"
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useRef, useState } from "react";
+import {
+  AddUnorderedElement,
+  GetUnorderedElements,
+  RemoveUnorderedElement,
+} from "../../wailsjs/go/main/App";
+import SettingBox from "../ui/SettingBox";
 
 function UserLines() {
-    const [elements, setElements] = useState<any>([])
-    const customLineInput = useRef<HTMLInputElement>(null)
+  const [elements, setElements] = useState<any>([]);
+  const customLineInput = useRef<HTMLInputElement>(null);
 
-    useEffect(() => {
-        reloadUnorderedElements()
-    }, [])
+  useEffect(() => {
+    reloadUnorderedElements();
+  }, []);
 
-    async function reloadUnorderedElements() {
-        const r = await GetUnorderedElements()
-        console.log(r)
-        setElements(r)
+  async function reloadUnorderedElements() {
+    const r = await GetUnorderedElements();
+    console.log(r);
+    setElements(r);
+  }
+
+  async function addLine() {
+    const value = customLineInput.current?.value;
+    if (value != "") {
+      await AddUnorderedElement("" + value);
+      reloadUnorderedElements();
+      customLineInput.current!.value = "";
     }
+  }
 
-    async function addLine() {
-        const value = customLineInput.current?.value
-        if (value != "") {
-            await AddUnorderedElement("" + value)
-            reloadUnorderedElements()
-            customLineInput.current!.value = ""
-        }
-    }
-
-    return (
-        <SettingBox header="Additional config" subheader="Add your own custom text into your configuration file!">
-            <div className="grid">
-                <div className="flex gap-3">
-                    <div>
-                        <form onSubmit={(event) => {
-                            event.preventDefault()
-                            addLine()
-                        }}>
-                            <input
-                                ref={customLineInput}
-                                type="text"
-                                className="bg-latte-surface0 dark:bg-mocha-surface0 border border-latte-surface2 dark:border-mocha-surface2 p-2 rounded w-60"
-                            />
-                        </form>
-                    </div>
-                    <div className="flex items-center">
-                        <a onClick={() => {
-                            addLine()
-                        }} className="cursor-pointer text-lg"><FontAwesomeIcon icon={faPlus} /></a>
-                    </div>
-                </div>
-                <div className="mt-3">
-                    {elements.map((e: any) => (
-                        <div key={e.Index} className="grid grid-cols-[1fr_auto]">
-                            <div>
-                                {e.Name}
-                            </div>
-                            <div>
-                                <a
-                                    className="cursor-pointer"
-                                    onClick={() => {
-                                        RemoveUnorderedElement(e.Index).then(() => {
-                                            reloadUnorderedElements()
-                                        })
-                                    }}
-                                >
-                                    <FontAwesomeIcon icon={faMinus} />
-                                </a>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <SettingBox
+      header="Additional config"
+      subheader="Add your own custom text into your configuration file!"
+    >
+      <div className="grid">
+        <div className="grid grid-cols-[1fr_auto] gap-3">
+          <div>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                addLine();
+              }}
+            >
+              <input
+                ref={customLineInput}
+                type="text"
+                className="bg-latte-surface0 dark:bg-mocha-surface0 border border-latte-surface2 dark:border-mocha-surface2 p-2 rounded w-full"
+              />
+            </form>
+          </div>
+          <div className="flex items-center">
+            <a
+              onClick={() => {
+                addLine();
+              }}
+              className="cursor-pointer text-lg"
+            >
+              <FontAwesomeIcon icon={faPlus} />
+            </a>
+          </div>
+        </div>
+        <div className="mt-3">
+          {elements.map((e: any) => (
+            <div key={e.Index} className="grid grid-cols-[1fr_auto]">
+              <div>{e.Name}</div>
+              <div>
+                <a
+                  className="cursor-pointer"
+                  onClick={() => {
+                    RemoveUnorderedElement(e.Index).then(() => {
+                      reloadUnorderedElements();
+                    });
+                  }}
+                >
+                  <FontAwesomeIcon icon={faMinus} />
+                </a>
+              </div>
             </div>
-        </SettingBox>
-    )
+          ))}
+        </div>
+      </div>
+    </SettingBox>
+  );
 }
 export default UserLines;
