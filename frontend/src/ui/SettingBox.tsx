ab98315ad508217e676d93f9629bcf75c1db8378
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-02-23 11:15:47 +0100

    no borders and headers rework

--- frontend/src/ui/SettingBox.tsx
+++ frontend/src/ui/SettingBox.tsx
@@ -1,26 +1,40 @@
 import { ReactNode } from "react";
 
 interface Props {
-    header: string;
-    subheader?: string;
-    classes?: string;
-    nomargin?: boolean;
-    children?: ReactNode;
-    noGap?: boolean;
+  header: string;
+  subheader?: string;
+  classes?: string;
+  nomargin?: boolean;
+  children?: ReactNode;
+  noGap?: boolean;
 }
-function SettingBox({ header, subheader, classes, nomargin, children, noGap }: Props) {
-    let marginBottom = "mb-3"
-    if (nomargin) {
-        marginBottom = ""
-    }
-    return (
-        <div className={marginBottom + classes}>
-            <h1 className="text-lg">{header}</h1>
-            <small>{subheader}</small>
-            <div className={"border-2 border-latte-surface2 dark:border-mocha-surface2 py-3 px-5 rounded flex flex-col " + (!noGap ? "gap-3" : "")}>
-                {children}
-            </div>
-        </div>
-    );
+function SettingBox({
+  header,
+  subheader,
+  classes,
+  nomargin,
+  children,
+  noGap,
+}: Props) {
+  let marginBottom = "mb-3";
+  if (nomargin) {
+    marginBottom = "";
+  }
+  return (
+    <div className={marginBottom + classes}>
+      <h1 className="text-lg first-letter:text-xl tracking-wide mb-0">
+        {header}
+      </h1>
+      <small className="mt-0">{subheader}</small>
+      <div
+        className={
+          "border-0 border-latte-surface2 dark:border-mocha-surface2 py-3 px-5 rounded flex flex-col " +
+          (!noGap ? "gap-3" : "")
+        }
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
 export default SettingBox;
