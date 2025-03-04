4f0075fdaec9c9b4aa7bb27371f73df7da45bf88
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-03-04 10:43:39 +0100

    order

--- frontend/src/pages/ExtraSettings.tsx
+++ frontend/src/pages/ExtraSettings.tsx
@@ -6,10 +6,14 @@ import Gpu from "../settings/Gpu";
 export default function ExtraSettings() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-      <Cpu />
-      <Fps />
-      <Gpu />
-      <Colors />
+      <div className="flex flex-col gap-5">
+        <Cpu />
+        <Gpu />
+        <Fps />
+      </div>
+      <div className="flex flex-col gap-5">
+        <Colors />
+      </div>
     </div>
   );
 }
