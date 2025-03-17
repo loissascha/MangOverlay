d3aa1b2c0b26c7866dffc33d6839483d0167efd5
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-03-17 21:15:40 +0100

    presets

--- frontend/src/pages/PresetsSettings.tsx
+++ frontend/src/pages/PresetsSettings.tsx
@@ -3,30 +3,48 @@ import preset1Img from "../assets/images/presets-1.png";
 export default function PresetsSettings() {
   return (
     <>
-      <div className="grid grid-cols-2 h-full">
-        <div className="border-r border-b p-2 border-latte-surface2 dark:border-mocha-surface2">
+      <div className="text-center mb-3 text-xl">
+        <strong>Attention!</strong> This will overwrite <strong>all</strong> of
+        your current settings!
+      </div>
+      <div className="grid grid-cols-2">
+        <div className="border-r border-b border-t p-2 border-latte-surface2 dark:border-mocha-surface2">
           <div className="text-center text-xl">Preset 1</div>
           <img src={preset1Img} className="mx-auto max-h-96" />
           <div className="text-center">
             <PresetButton>Apply</PresetButton>
           </div>
         </div>
-        <div className="border-b p-2 border-latte-surface2 dark:border-mocha-surface2">
+        <div className="border-b border-t p-2 border-latte-surface2 dark:border-mocha-surface2">
           <div className="text-center text-xl">Preset 2</div>
           <img src={preset1Img} className="mx-auto max-h-96" />
           <div className="text-center">
             <PresetButton>Apply</PresetButton>
           </div>
         </div>
-        <div className="border-r p-2 border-latte-surface2 dark:border-mocha-surface2">
+        <div className="border-r border-b p-2 border-latte-surface2 dark:border-mocha-surface2">
           <div className="text-center text-xl">Preset 3</div>
           <img src={preset1Img} className="mx-auto max-h-96" />
           <div className="text-center">
             <PresetButton>Apply</PresetButton>
           </div>
         </div>
-        <div className="p-2">
+        <div className="border-b p-2 border-latte-surface2 dark:border-mocha-surface2">
           <div className="text-center text-xl">Preset 4</div>
+          <img src={preset1Img} className="mx-auto max-h-96" />
+          <div className="text-center">
+            <PresetButton>Apply</PresetButton>
+          </div>
+        </div>
+        <div className="border-r p-2 border-latte-surface2 dark:border-mocha-surface2">
+          <div className="text-center text-xl">Preset 5</div>
+          <img src={preset1Img} className="mx-auto max-h-96" />
+          <div className="text-center">
+            <PresetButton>Apply</PresetButton>
+          </div>
+        </div>
+        <div className="p-2">
+          <div className="text-center text-xl">Preset 6</div>
           <img src={preset1Img} className="mx-auto max-h-96" />
           <div className="text-center">
             <PresetButton>Apply</PresetButton>
@@ -42,7 +60,7 @@ interface ButtonProps {
 }
 function PresetButton({ children }: ButtonProps) {
   return (
-    <button className="py-2 px-3 bg-latte-surface0 dark:bg-mocha-surface0 rounded border border-latte-surface2 dark:border-mocha-surface2 cursor-pointer hover:bg-latte-surface1 dark:hover:bg-mocha-surface2">
+    <button className="py-2 px-3 mb-2 bg-latte-surface0 dark:bg-mocha-surface0 rounded border border-latte-surface2 dark:border-mocha-surface2 cursor-pointer hover:bg-latte-surface1 dark:hover:bg-mocha-surface2">
       {children}
     </button>
   );
