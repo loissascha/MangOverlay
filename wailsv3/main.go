f379e7b063ba6d190b716bb19a2c403b3e9af77b
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-08-03 09:43:35 +0200

    titles

--- wailsv3/main.go
+++ wailsv3/main.go
@@ -35,8 +35,8 @@ func main() {
 	// 'Bind' is a list of Go struct instances. The frontend has access to the methods of these instances.
 	// 'Mac' options tailor the application when running an macOS.
 	app := application.New(application.Options{
-		Name:        "wailsv3",
-		Description: "A demo of using raw HTML & CSS",
+		Name:        "MangOverlay",
+		Description: "",
 		Services: []application.Service{
 			application.NewService(&GreetService{}),
 		},
@@ -54,7 +54,7 @@ func main() {
 	// 'BackgroundColour' is the background colour of the window.
 	// 'URL' is the URL that will be loaded into the webview.
 	app.Window.NewWithOptions(application.WebviewWindowOptions{
-		Title: "Window 1",
+		Title: "MangOverlay",
 		// Window sized to the golden ratio (1000 / 618 ≈ 1.618).
 		Width:  1000,
 		Height: 618,
